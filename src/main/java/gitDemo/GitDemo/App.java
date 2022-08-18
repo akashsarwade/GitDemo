8b9603214808ae42bccc5d5868eb1857e763114d
Akash Sarwade <akshsarwade@gmail.com>
2022-08-18 17:07:57 +0530

    Demo project added

---
+++ src/main/java/gitDemo/GitDemo/App.java
@@ -0,0 +1,14 @@
+package gitDemo.GitDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Welcome to git Demo Project");
+    }
+}
